4cf8129cca1941cba848ca27a954b56f76b6750a
suzuki-gunji <gunji1102@gmail.com>
2021-03-12 11:12:32 +0900

    ツイートの投稿機能の追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,18 @@ class TweetsController < ApplicationController
   def index 
     @tweets = Tweet.all
   end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.create(tweet_params)
+    redirect_to root_path
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:title, :image, :text)
+  end
 end
