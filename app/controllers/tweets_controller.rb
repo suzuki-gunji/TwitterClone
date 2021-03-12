5ad11e8d6bb72bdccfd74fb551856aea1f962cd8
suzuki-gunji <gunji1102@gmail.com>
2021-03-12 14:23:10 +0900

    tweetした際にcurrent_user.idを保存する機能の追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -34,7 +34,7 @@ class TweetsController < ApplicationController
 
   private
   def tweet_params
-    params.require(:tweet).permit(:title, :image, :text)
+    params.require(:tweet).permit(:name, :image, :text).merge(user_id: current_user.id)
   end
 
   def set_tweet
